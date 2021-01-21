c37365b0238523adfe9822126374152155bf236e
linus muema <linus.m.muema@gmail.com>
2021-01-21 16:41:23 +0300

    - add platform channel

--- android/app/src/main/kotlin/com/moose/flutter_play/MainActivity.kt
+++ android/app/src/main/kotlin/com/moose/flutter_play/MainActivity.kt
@@ -1,19 +1,26 @@
 package com.moose.flutter_play
 
-import android.os.Bundle
-import android.os.PersistableBundle
 import android.view.View
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
+    private val channel = "com.moose.traveller/flags"
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
-        GeneratedPluginRegistrant.registerWith(this.flutterEngine!!)
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, channel).setMethodCallHandler {
+            call, result ->
+            if (call.method == "setFlags"){
+                window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+                result.success(true)
+            }
+            else result.notImplemented()
+        }
     }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_play/screens/home.dart';
 import 'package:flutter_play/screens/splash.dart';
-import 'package:fullscreen/fullscreen.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,21 +11,11 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  FullScreen fullscreen;
-
-  @override
-  void initState() {
-    super.initState();
-    fullscreen = new FullScreen();
-    enterFullScreen(FullScreenMode.EMERSIVE_STICKY);
-  }
-
-  void enterFullScreen(FullScreenMode fullScreenMode) async {
-    await fullscreen.enterFullScreen(fullScreenMode);
-  }
+  static const platform = MethodChannel('com.moose.traveller/flags');
 
   @override
   Widget build(BuildContext context) {
+    platform.invokeMethod('setFlags');
     return MaterialApp(
       theme: ThemeData(primaryColor: Colors.white),
       initialRoute: '/',
