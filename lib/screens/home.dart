0876d1eb13b9e91f6c51fa613d3d02de6617308c
linus muema <linus.m.muema@gmail.com>
2021-01-25 10:29:30 +0300

    - finish home screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:flutter_play/components/colors.dart';
 import 'package:flutter_play/components/country.dart';
 import 'package:flutter_play/components/texts.dart';
@@ -25,24 +26,24 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
         children: [
           Container(
               child: Text(heading, style: h2(Colors.white)),
-              padding: EdgeInsets.only(top: 65, bottom: 2, left: 25)),
+              margin: EdgeInsets.only(left: 25, top: 65, bottom: 2)),
           Container(
             child: Text(subheading, style: normal(Colors.white)),
-            padding: EdgeInsets.only(left: 25),
-            margin: EdgeInsets.only(bottom: 45),
+            margin: EdgeInsets.only(left: 25, top: 2, bottom: 45),
           ),
           Expanded(
             child: CarouselSlider(
-                items: countries,
-                options: CarouselOptions(
-                  height: MediaQuery.of(context).size.longestSide,
-                  autoPlay: true,
-                  onPageChanged: (index, _) {
-                    setState(() {
-                      _currentIndex = index;
-                    });
-                  },
-                )),
+              items: countries,
+              options: CarouselOptions(
+                height: MediaQuery.of(context).size.longestSide,
+                autoPlay: true,
+                onPageChanged: (index, _) {
+                  setState(() {
+                    _currentIndex = index;
+                  });
+                },
+              ),
+            ),
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -50,14 +51,14 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
               final index = countries.indexOf(e);
               final active = _currentIndex == index;
               return Container(
-                margin:
-                    EdgeInsets.only(top: 40, bottom: 100, left: 5, right: 5),
+                margin: EdgeInsets.only(top: 30, bottom: 90, left: 4, right: 4),
                 width: active ? 20 : 10,
                 height: 5,
                 decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    shape: BoxShape.rectangle,
-                    color: active ? Colors.white : grey300),
+                  borderRadius: BorderRadius.circular(10),
+                  shape: BoxShape.rectangle,
+                  color: active ? Colors.white : grey300,
+                ),
               );
             }).toList(),
           )
