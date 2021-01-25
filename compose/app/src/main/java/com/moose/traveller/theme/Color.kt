0a30f7ecf17e33c2f9cc454e27816c7fd9ffafe1
linus muema <linus.m.muema@gmail.com>
2021-01-25 10:46:27 +0300

    - removed compose dir

--- compose/app/src/main/java/com/moose/traveller/theme/Color.kt
+++
@@ -1,6 +0,0 @@
-package com.moose.traveller.theme
-
-import androidx.compose.material.lightColors
-import androidx.compose.ui.graphics.Color
-
-val colors = lightColors(primary = Color.White, onPrimary = Color.White)
